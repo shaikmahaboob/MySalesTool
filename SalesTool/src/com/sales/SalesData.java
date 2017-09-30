66e7e9704c6593da94c0abcf5cbbb1a154fbf432
shaikmahaboob <subani.shaik725@gmail.com>
2017-09-30 16:26:11 +0530

    this first commit of sales tool.

---
+++ SalesTool/src/com/sales/SalesData.java
@@ -0,0 +1,9 @@
+package com.sales;
+
+public class SalesData {
+
+public void data()	
+{
+	System.out.println("this data from data method() of SalesData class");
+}
+}
---
+++ SalesTool/src/com/sales/SalesDemo.java
@@ -0,0 +1,17 @@
+package com.sales;
+
+public class SalesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data=new SalesData();
+		data.data();
+		
+
+	}
+
+	public static void display()
+	{
+		System.out.println("Hello happy sales people");
+	}
+}
